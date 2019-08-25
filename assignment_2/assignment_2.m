fb7b4bfb43fe89a2ef2de9228b9cde8a38e52114
Madrajib Lab <madhabsharma94@gmail.com>
2019-08-25 21:40:32 +0530

    Add files via upload

---
+++ assignment_2/assignment_2.m
@@ -0,0 +1,31 @@
+noelt=zeros(1,10);
+nocp=zeros(1,10);
+nocp_1=zeros(1,10);
+nocp_2 = zeros(1,10);
+k=1;
+for n=10:10:100
+    noelt(k)=n;
+    
+    av_cp_1 = 0;
+    av_cp_2 = 0;
+    for i=1:10
+        a=round(rand(1,n)*100)
+        av_cp_1 = av_cp_1 + bubbleSort(a)
+        av_cp_2 = av_cp_2 + bubbleSortOp(a)
+    end
+    
+    nocp(k) = av_cp_1 / 10;
+    nocp_1(k) = av_cp_2 / 10;
+       
+    k=k+1
+end
+
+
+plot(noelt,nocp,noelt,nocp_1)
+xlabel('#elements')
+ylabel('#comparisons')
+legend('bubbleSort','bubble sort optimal')
+
+
+grid on
+
