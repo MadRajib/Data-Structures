9f44f4b34b0bc358cb32a19eb13a4f9d8fc1f5f1
Madrajib Lab <madhabsharma94@gmail.com>
2019-08-25 22:03:11 +0530

    Update assignment_6.m

--- assignment_6/assignment_6.m
+++ assignment_6/assignment_6.m
@@ -3,10 +3,10 @@ nocp=zeros(1,10);
 k=1;
 for n=50:10:500
     noelt(k)=n
-    
+    cmp =0
     for i=1:10
       a=round(rand(1,n)*100)
-      cmp=quickselect(a,3)
+      cmp= cmp + quickselect(a,3)
     end
     
     nocp(k)=cmp/10
